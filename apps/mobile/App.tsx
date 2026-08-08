21d98662499d89fe3c2b7fb1bdfeb81e89d79ec5
Lirxstar <163003848+Lirxstar@users.noreply.github.com>
2026-08-08 21:31:41 +0800

    Show public demo disclosure in deployed web app

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -2,10 +2,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { AppNavigator } from "./src/AppNavigator";
 import { JourneyProvider } from "./src/journey/JourneyContext";
+import { PublicDemoNotice } from "./src/PublicDemoNotice";
 
 export default function App() {
   return (
     <SafeAreaProvider>
+      <PublicDemoNotice />
       <JourneyProvider>
         <AppNavigator />
       </JourneyProvider>
